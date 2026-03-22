50152af8e8ebbb8b2bf47448ae59208009661168
Isac Costa <isaccostadasilva@gmail.com>
2026-03-22 17:45:08 -0300

    changes for deployment 3

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ app.use(cookieParser()); // Middleware para analisar cookies
 
 // Configuração do CORS (permitir apenas domínios específicos)
 app.use(
-  cors()
+  cors({
+    origin: "https://hubmain.com.br",
+    credentials: true, // se for usar cookies/autenticação no futuro
+  })
 );
 
 // Roteamento principal
